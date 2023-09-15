75d0e4004d0ba030e6a9f91af1d34762bc70249c
Vinodkadvade <141844414+Vinodkadvade@users.noreply.github.com>
2023-09-15 15:15:08 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "which is the highest dam of india?",
+    answer: "Tehri Dam",
+    options: [
+      " Rihand Dam",
+      "Tehri Dam",
+      "Mettur Dam",
+      "Sardar sarover dam"
+    ]
+  },
+    {
+    numb: 2,
+    question: "how many days are there in august?",
+    answer: "31",
+    options: [
+     "30",
+     "31",
+     "29",
+     "28"
+    ]
+  },
+    {
+    numb: 3,
+    question: "how many colour are there in rainbow?",
+    answer: "7",
+    options: [
+      "10",
+      "7",
+      "9",
+      "11"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Taj Mahal is located at which place?",
+    answer: "Agra",
+    options: [
+      "Dehradun",
+      "Ranchi",
+      "Mumbai",
+      "Agra"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is sun?",
+    answer: "star",
+    options: [
+      "Planet",
+      "Comet",
+      "Pulsar",
+      "star"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
